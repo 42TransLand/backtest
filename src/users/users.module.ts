fc5ad7219f1c50757fe0f66c6b743f5c1c69379b
jiholee <stellar7484@naver.com>
2022-07-19 13:51:41 +0900

    custom 폴더로 이동

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,37 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { USERS_CUSTOM_REPOSITORY } from "./users.decorator";
 import { getDataSourceToken } from "@nestjs/typeorm";
 import { DataSource } from "typeorm";
-
-export class TypeOrmExModule {
-  public static forCustomRepository<T extends new (...args: any[]) => any>(repositories: T[]): DynamicModule {
-    const providers: Provider[] = [];
-
-    for (const repository of repositories) {
-      const entity = Reflect.getMetadata(USERS_CUSTOM_REPOSITORY, repository);
-
-      if (!entity) {
-        continue;
-      }
-      providers.push({
-        inject: [getDataSourceToken()],
-        provide: repository,
-        useFactory: (dataSource: DataSource): typeof repository => {
-          const baseRepository = dataSource.getRepository<any>(entity);
-          return new repository(baseRepository.target, baseRepository.manager, baseRepository.queryRunner);
-        },
-      });
-    }
-
-    return {
-      exports: providers,
-      module: TypeOrmExModule,
-      providers,
-    };
-  }
-}
+import { TypeOrmExModule } from 'src/custom/typeorm.module';
 
 @Module({
   imports: [
--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,6 +1,6 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./entities/user.entity";
-import { CustomRepository } from "./users.decorator"
+import { CustomRepository } from "../custom/typeorm.decorator"
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
